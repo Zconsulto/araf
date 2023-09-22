57228f54d9f38a0c353e9ee704a0a82063cc51ea
Zconsulto <141072452+Zconsulto@users.noreply.github.com>
2023-09-22 14:18:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function sendEmail() {
+    const userEmail = document.getElementById("email").value;
+    const subject = "Customer wants to Contact US";
+
+    fetch('http://localhost:3000/send-email', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: 'ashorbagy@zconsulto.com',
+            subject: subject,
+            message: userEmail  
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Email sending failed. Please try again later.');
+    });
+}
